5ee703483ed8209bf9c2f3ede74a6dda878a3179
Sushma Acharya <sushmaacharya345@gmail.com>
2024-08-22 15:54:56 +0545

    complete basic frontend design

--- client/src/Components/about/about.tsx
+++ client/src/Components/about/about.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Grid, Container, Typography } from '@mui/material';
 import Image from 'next/image'
-import SushmaProfile from '@/Components/Images/Sushma.jpeg'
+import SushmaProfile from '@/Components/Images/Sushma1.jpeg'
 import './about.css'
+
 const About: React.FC = () => {
 	return (
 		<React.Fragment>
@@ -13,12 +14,12 @@ const About: React.FC = () => {
 				alignItems="center"
 				>
 				<div className="about_me">
-					<div className="MY-Info">
+					<div className='profile-pic'>
+						<Image src={SushmaProfile} alt='Sushma Acharya' />
+					</div>
+					<div className="my-info">
 					<Typography variant="h1">SUSHMA ACHARYA</Typography>
 					<Typography variant="h6">PASSIONATE WEB DEVELOPER FROM NEPAL</Typography>
-					</div>
-					<div className='Profile-pic'>
-						<Image src={SushmaProfile} alt='Sushma Acharya' />
 					</div>
 				</div>
 			</Grid>
